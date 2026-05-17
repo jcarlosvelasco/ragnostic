ac7091f3f1e41fa5585a019e239c3d2637216e97
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2026-05-17 17:26:29 +0200

    Adds docker compose

--- src/docs/embedder.py
+++ src/docs/embedder.py
@@ -1,41 +1,10 @@
-import hashlib
+import json
 
-from qdrant_client import QdrantClient
-from qdrant_client.models import Distance, PointStruct, VectorParams
-
-
-def getClient() -> QdrantClient:
-    client = QdrantClient(url="http://localhost:6333")
-    return client
+import requests
 
 
-client = getClient()
-
-
-def create_vector_store(client: QdrantClient, collection_name: str, size: int):
-    if client.collection_exists(collection_name):
-        return
-
-    client.create_collection(
-        collection_name=collection_name,
-        vectors_config=VectorParams(size=size, distance=Distance.DOT),
-    )
-
-
-def content_to_id(text: str) -> str:
-    return hashlib.md5(text.encode()).hexdigest()
-
-
-# Falta definir payload, puede tener campos text: str, source: str (nombre del documento), chunk: int (numero de chunk)
-
-
-async def append_vector(
-    client: QdrantClient, collection_name: str, vector: list[float], payload: dict
-):
-    point_id = content_to_id(payload["text"])
-
-    client.upsert(
-        collection_name=collection_name,
-        wait=True,
-        points=[PointStruct(id=point_id, vector=vector, payload=payload)],
-    )
+def convert_to_embedding(text: str) -> list[float]:
+    url = "http://ollama:11434/api/embeddings"
+    data = {"model": "nomic-embed-text", "prompt": text}
+    response = requests.post(url, json=data)
+    return json.loads(response.text)["embedding"]
